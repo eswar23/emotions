44bda92108402e52376e137a2e107f3f161da786
eswar23 <kartzeswar@gmail.com>
2017-07-23 17:25:59 +0800

    Update emotions.py

--- emotions.py
+++ emotions.py
@@ -60,9 +60,8 @@ def webhook():
         # "contextOut": [],
         "source": "emotions"
     }
-    if __name__ == '__main__':
-        port = int(os.getenv('PORT', 5000))
-
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
     print("Starting app on port %d" % port)
 
     app.run(debug=False, port=port, host='0.0.0.0')
